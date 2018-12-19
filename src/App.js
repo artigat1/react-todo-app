1d5f3c89b814d39004792edd6d0ad3186aa5a1e1
Steve Doggett <artigat1@users.noreply.github.com>
2018-12-19 13:20:39 +0000

    Initial commit
    This is the result of following this tutorial to demo GraphQL with React and AWS AppSync: https://egghead.io/lessons/react-course-overview-scalable-offline-ready-graphql-applications-with-aws-appsync-react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import gql from "graphql-tag";
+import { compose, graphql } from "react-apollo";
+import { graphqlMutation } from "aws-appsync-react";
+import { buildSubscription } from "aws-appsync";
+
+const SubscribeToTodos = gql`
+  subscription {
+    onCreateTodo {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const CreateTodo = gql`
+  mutation($title: String!, $completed: Boolean) {
+    createTodo(input: { title: $title, completed: $completed }) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const ListTodos = gql`
+  query {
+    listTodos {
+      items {
+        id
+        title
+        completed
+      }
+    }
+  }
+`;
 
 class App extends Component {
+  state = { todo: "" };
+
+  componentDidMount() {
+    this.props.subscribeToMore(buildSubscription(SubscribeToTodos, ListTodos));
+  }
+
+  addTodo = () => {
+    if (this.state.todo === "") return;
+    const todo = {
+      title: this.state.todo,
+      completed: false
+    };
+    this.props.createTodo(todo);
+    this.setState({ todo: "" });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main>
+          <input
+            onChange={e => this.setState({ todo: e.target.value })}
+            value={this.state.todo}
+            placeholder="Todo name"
+          />
+          <button onClick={this.addTodo}>Add Todo</button>
+          <ul>
+            {this.props.todos.map((item, i) => (
+              <li key={item.id}>{item.title}</li>
+            ))}
+          </ul>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default compose(
+  graphqlMutation(CreateTodo, ListTodos, "Todo"),
+  graphql(ListTodos, {
+    options: {
+      fetchPolicy: "cache-and-network"
+    },
+    props: props => ({
+      subscribeToMore: props.data.subscribeToMore,
+      todos: props.data.listTodos ? props.data.listTodos.items : []
+    })
+  })
+)(App);
